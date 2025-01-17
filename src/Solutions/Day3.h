49aac9a5339e3c2d717a052336e9acdcfe2776e1
Christian <c-kj@mail.dk>
2025-01-17 18:48:15 +0100

    Day3 setup

---
+++ src/Solutions/Day3.h
@@ -0,0 +1,25 @@
+#ifndef DAY3_H
+#define DAY3_H
+
+#include <string_view>
+
+class Day3
+{
+public:
+    Day3() = default;
+
+    void Part1() const
+    {
+
+    }
+
+    void Part2() const
+    {
+    }
+
+private:
+    std::string_view input_test{ R"(..\src\Input\day3_test.txt)"};
+    std::string_view input_data{ R"(..\src\Input\day3.txt)"};
+};
+
+#endif //DAY3_H
