b1b00464636812cea0178b48abcee55023569634
Christian <c-kj@mail.dk>
2025-01-17 20:35:34 +0100

    Day4 setup

---
+++ src/Solutions/Day4.h
@@ -0,0 +1,24 @@
+#ifndef DAY4_H
+#define DAY4_H
+
+#include <string_view>
+
+class Day4
+{
+public:
+    Day4() = default;
+
+    void Part1() const
+    {
+    }
+
+    void Part2() const
+    {
+    }
+
+private:
+    std::string_view input_test{ R"(..\src\Input\day4_test.txt)"};
+    std::string_view input_data{ R"(..\src\Input\day4.txt)"};
+};
+
+#endif //DAY4_H
