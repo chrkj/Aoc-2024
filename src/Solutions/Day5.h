789bf43755412682ee6a6539dbe0f036e93b21b2
Christian <c-kj@mail.dk>
2025-01-18 18:54:46 +0100

    Day5 setup

---
+++ src/Solutions/Day5.h
@@ -0,0 +1,27 @@
+#ifndef DAY5_H
+#define DAY5_H
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <string_view>
+
+class Day5
+{
+public:
+    Day5() = default;
+
+    void Part1()
+    {
+    }
+
+    void Part2() const
+    {
+    }
+
+private:
+    std::string_view input_test{ R"(..\src\Input\day5_test.txt)"};
+    std::string_view input_data{ R"(..\src\Input\day5.txt)"};
+};
+
+#endif //DAY5_H
