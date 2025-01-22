f1232ce58e734ddc075cd0d96d223a4e2b4d2e53
Christian <c-kj@mail.dk>
2025-01-22 23:51:51 +0100

    Day7

---
+++ src/Solutions/Day7.h
@@ -0,0 +1,24 @@
+#ifndef DAY7_H
+#define DAY7_H
+
+#include <string_view>
+
+class Day7
+{
+public:
+    Day7() = default;
+
+    void Part1()
+    {
+    }
+
+    void Part2()
+    {
+    }
+
+private:
+    std::string_view input_test{R"(..\src\Input\day7_test.txt)"};
+    std::string_view input_data{R"(..\src\Input\day7.txt)"};
+};
+
+#endif //DAY7_H
