cb2f3058dd58269643a3813d8b5512af571fffb0
Christian Kjær <c-kj@mail.dk>
2025-01-28 16:42:28 +0100

    Day 7 part 2

--- src/Solutions/Day7.h
+++ src/Solutions/Day7.h
@@ -1,6 +1,7 @@
 #ifndef DAY7_H
 #define DAY7_H
 
+#include <cmath>
 #include <cstdint>
 #include <string_view>
 
@@ -11,14 +12,28 @@ class Day7
 public:
     Day7() = default;
 
-    bool IsPossible(const std::vector<uint64_t>& equation, const uint64_t runningSum, const uint16_t i)
+    uint64_t Concatenation(const uint64_t left, const uint64_t right)
     {
-        if(runningSum == equation[0] && i == equation.size()) return true;
-        if(i >= equation.size() || runningSum > equation[0])  return false;
+        int digits = 0;
+        uint64_t temp = right;
+        while (temp > 0)
+        {
+            temp /= 10;
+            ++digits;
+        }
+        return left * static_cast<long>(std::pow(10, digits)) + right;
+    }
 
-        if(IsPossible(equation, runningSum + equation[i], i+1))
+    bool IsPossible(const std::vector<uint64_t>& equation, const uint64_t runningSum, const int i, const bool part2)
+    {
+        if (runningSum == equation[0] && i == equation.size()) return true;
+        if (i >= equation.size() || runningSum > equation[0])  return false;
+
+        if (IsPossible(equation, runningSum + equation[i], i+1, part2))
             return true;
-        if(IsPossible(equation, runningSum * equation[i], i+1))
+        if (IsPossible(equation, runningSum * equation[i], i+1, part2))
+            return true;
+        if (IsPossible(equation, Concatenation(runningSum, equation[i]), i+1, part2) && part2)
             return true;
 
         return false;
@@ -29,13 +44,19 @@ public:
         uint64_t sum = 0;
         const auto equations = Utils::ParseToVectorVector<uint64_t>(input_data, ' ');
         for (auto equation : equations)
-            if (IsPossible(equation, equation[1], 2))
+            if (IsPossible(equation, equation[1], 2, false))
                 sum += equation[0];
         printf("Sum: %llu\n", sum);
     }
 
     void Part2()
     {
+        uint64_t sum = 0;
+        const auto equations = Utils::ParseToVectorVector<uint64_t>(input_data, ' ');
+        for (auto equation : equations)
+            if (IsPossible(equation, equation[1], 2, true))
+                sum += equation[0];
+        printf("Sum: %llu\n", sum);
     }
 
 private:
