95cbf2a82a502f3d176495365b8e521b6455ab35
Christian <c-kj@mail.dk>
2025-02-09 01:21:29 +0100

    Day 9 added

---
+++ src/Solutions/Day9.h
@@ -0,0 +1,24 @@
+#ifndef DAY9_H
+#define DAY9_H
+
+#include <string_view>
+
+class Day9
+{
+public:
+    Day9() = default;
+
+    void Part1()
+    {
+    }
+
+    void Part2()
+    {
+    }
+
+private:
+    std::string_view input_test{R"(..\src\Input\day9_test.txt)"};
+    std::string_view input_data{R"(..\src\Input\day9.txt)"};
+};
+
+#endif //DAY9_H
