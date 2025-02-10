831ab99f5abfa9346f4ecbf2b28a6db3a9c473f9
Christian <c-kj@mail.dk>
2025-02-10 15:20:25 +0100

    Day9 part 2 wip

--- src/Solutions/Day9.h
+++ src/Solutions/Day9.h
@@ -21,6 +21,7 @@ public:
         std::vector<int> expandedMap;
         for (auto value: diskmap)
         {
+
             for (int i = 0; i < value - '0'; ++i)
             {
                 if (isFreeSpace)
@@ -64,12 +65,73 @@ public:
 
     void Part2()
     {
+        Utils::Timer timer;
+        const auto diskmap = Utils::ParseToVector<char>(input_test);
 
+        // Expand
+        int id = 0;
+        bool isFreeSpace = false;
+        std::vector<File> fileMap;
+        for (const auto size: diskmap)
+        {
+            File f(id, size - '0', isFreeSpace);
+            if (isFreeSpace)
+                f.id = -1;
+            if (size != '0')
+                fileMap.push_back(f);
+            if (!isFreeSpace)
+                id++;
+            isFreeSpace = !isFreeSpace;
+        }
+
+        // Compress
+        for (int r = fileMap.size()-1; r > 0; --r)
+        {
+            for (int l = 0; l < r; ++l)
+            {
+                if (fileMap[l].isFreeSpace && !fileMap[r].isFreeSpace && fileMap[l].size >= fileMap[r].size)
+                {
+                    if (fileMap.size() == fileMap[r].size)
+                        iter_swap(fileMap.begin() + l, fileMap.begin() + r);
+                    else
+                    {
+                        const int newSize = fileMap[l].size - fileMap[r].size;
+                        iter_swap(fileMap.begin() + l, fileMap.begin() + r);
+                        fileMap[r].size = fileMap[l].size;
+                        fileMap.insert(fileMap.begin()+l+1, File(-1,newSize,true));
+                    }
+                    break;
+                }
+            }
+        }
+
+        // Checksum
+        int idd = 0;
+        long long checksum = 0;
+        for (const auto file: fileMap)
+        {
+            if (file.size == 0) continue;
+            for (int i = idd; i < file.size + idd; ++i)
+            {
+                printf("%d * %d \n", i, file.id);
+                if (file.id != -1)
+                    checksum += i * file.id;
+            }
+            idd += file.size;
+        }
+        printf("Checksum: ""%lld\n", checksum);
     }
 
 private:
     std::string_view input_test{R"(..\src\Input\day9_test.txt)"};
     std::string_view input_data{R"(..\src\Input\day9.txt)"};
+
+    struct File
+    {
+        int id;
+        int size;
+        bool isFreeSpace;
+    };
 };
 
 #endif //DAY9_H
